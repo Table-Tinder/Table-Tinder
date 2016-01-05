22571c733713753eba2290a137f0a0a0a7ad4991
Jason Tham <jtham@sbcglobal.net>
2016-01-05 13:54:34 -0600

    started api building - added appointment and diner controllers, added routing in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var ctrlDiners = require('../controllers/diners');
+var ctrlAppoinments = require('../controllers/appointments')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET all appointments. */
+router.get('/appointments', function(req, res) {
+
 });
 
 module.exports = router;
