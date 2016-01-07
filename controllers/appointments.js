7f72c17d0b3106c53456587cd032398b29d7b257
Jason Tham <jtham@sbcglobal.net>
2016-01-06 21:42:24 -0600

    added appointment controller in index.js route

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -74,3 +74,16 @@ module.exports.appointmentUpdate = function(req, res){
     sendJSON(res, 200, appointment);
   })
 }
+
+module.exports.showAppointment = function(req, res){
+  var theAppointment;
+  Appointment.findById(req.params.id, function(err, appointment){
+    if (err){
+      console.log("There was an error of: " + err);
+    }
+    console.log("success");
+    theAppointment = appointment;
+    console.log(theAppointment);
+  });
+  res.render('single', {title: 'Appointment', appointmentID: req.params.id, appt: theAppointment});
+}
